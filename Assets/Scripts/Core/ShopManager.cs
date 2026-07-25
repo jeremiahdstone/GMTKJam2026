a8093760956cbc9198625456b3c2cea99f667b49
KAMISENHEIMER <therealspydude@gmail.com>
2026-07-24 21:11:36 -0500

    added function to initialize the shop item panels

--- Assets/Scripts/Core/ShopManager.cs
+++ Assets/Scripts/Core/ShopManager.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+
 
 public class ShopManager : MonoBehaviour
 {
@@ -9,6 +13,11 @@ public class ShopManager : MonoBehaviour
     public TrapDatabase trapDatabase;
 
     public List<IShoppable> shopDatabase;
+
+    [Header("Shop UI")]
+    [SerializeField] private Transform shopRect;
+    [SerializeField] private GameObject upgradePanelPrefab;
+    [SerializeField] private GameObject trapPanelPrefab;
 
     void Start()
     {
@@ -22,13 +31,61 @@ public class ShopManager : MonoBehaviour
         {
             shopDatabase[0].OnPurchase();
         }
+
+        //CALL THIS WHENEVER YOU WANNA GENERATE AND DISPLAY A NEW SHOP
+        GenerateShop();
+
     }
 
-    public IShoppable[] generateShop() {
+    public void GenerateShop() {
         IShoppable[] shopList = new IShoppable[3];
 
+        //TODO eventually guarantee theres at least 1 trap and 1 upgrade
+        
+        //pick shop items
+        for (int i = 0; i < 3; i++)
+        {
+            shopList[i] = shopDatabase[Random.Range(0, shopDatabase.Count)];
+        }
 
+        Debug.Log(shopList[0]);
+        Debug.Log(shopList[1]);
+        Debug.Log(shopList[2]);
 
-        return shopList; 
-    } 
+        DisplayShop(shopList);
+    }
+
+    private void DisplayShop(IShoppable[] shopList)
+    {
+        // Remove old shop items
+        foreach (Transform child in shopRect)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (IShoppable item in shopList)
+        {
+            GameObject panel;
+
+            // if (item is Trap)
+            //     panel = Instantiate(trapPanelPrefab, shopRect);
+            // else
+            //     panel = Instantiate(upgradePanelPrefab, shopRect);
+
+            panel = Instantiate(upgradePanelPrefab, shopRect);
+
+            panel.transform.Find("Title")
+                .GetComponent<TextMeshProUGUI>().text = item.getName();
+
+            panel.transform.Find("Description")
+                .GetComponent<TextMeshProUGUI>().text = item.getDescription();
+
+            panel.transform.Find("upgrade/icon")
+                .GetComponent<Image>().sprite = item.getIcon();
+
+            panel.transform.Find("Button/PriceText")
+                .GetComponent<TextMeshProUGUI>().text = item.getCost().ToString();
+        }
+    }
+
 }
