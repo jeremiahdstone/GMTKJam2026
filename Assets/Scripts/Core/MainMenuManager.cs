9b08d3cf8ac269372f0cb1b801d61e2883b8ada4
Jeremiah Stone <chezwizzproductions@gmail.com>
2026-07-25 22:52:52 -0500

    logo and bats added to main menu

--- Assets/Scripts/Core/MainMenuManager.cs
+++ Assets/Scripts/Core/MainMenuManager.cs
@@ -1,21 +1,38 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject gameLogo;
+
+    [Header("Logo Bob")]
+    [SerializeField] private float bobDistance = 8f;
+    [SerializeField] private float bobDuration = 2.5f;
+
+    private Tween logoTween;
+    private RectTransform logoRect;
+
+    private void Start()
     {
-        
+        logoRect = gameLogo.GetComponent<RectTransform>();
+
+        logoTween = logoRect
+            .DOAnchorPosY(
+                logoRect.anchoredPosition.y + bobDistance,
+                bobDuration
+            )
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetUpdate(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void startGame()
+    public void StartGame()
     {
         SceneFader.instance.FadeToScene("GameScene");
+    }
+
+    private void OnDestroy()
+    {
+        logoTween?.Kill();
     }
 }
