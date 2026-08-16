9d49f8c52f919a67b388cf4657c5eedda6ffa733
Jeremiah Stone <chezwizzproductions@gmail.com>
2026-08-16 18:18:18 -0500

    implemented AOEBehavior with torchfarmer

--- Assets/Scripts/Enemy/AOEEnemy.cs
+++ Assets/Scripts/Enemy/AOEEnemy.cs
@@ -4,16 +4,28 @@ using System.Collections;
 
 public class AOEEnemy : Enemy
 {
+    [Header("AOE Enemy Settings")]
+    [SerializeField] private float radius;
+    [SerializeField] private AOEBehavior aoeBehavior;
 
-    [SerializeField] private GameObject aoeObject;
+    public override void Awake()
+    {
+        base.Awake();
+        aoeBehavior = GetComponentInChildren<AOEBehavior>();
+        aoeBehavior.RefreshVisual(radius);
+    }
     public override void Freeze(float cooldown, GameObject attacker = null)
     {
         base.Freeze(cooldown, attacker);
+
+        aoeBehavior.Hide();
         
     }
 
     public override void Unfreeze()
     {
         base.Unfreeze();
+
+        aoeBehavior.Show();
     }
 }
