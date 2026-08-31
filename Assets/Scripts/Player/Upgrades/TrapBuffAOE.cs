aaf4a0965e2d82e1b9150d85b75378e11affe1fe
KAMISENHEIMER <therealspydude@gmail.com>
2026-08-31 17:50:35 -0500

    reworked trap buff aoe upgrade % to be based on upgrade level

--- Assets/Scripts/Player/Upgrades/TrapBuffAOE.cs
+++ Assets/Scripts/Player/Upgrades/TrapBuffAOE.cs
@@ -2,49 +2,47 @@ using UnityEngine;
 
 public class TrapBuffAOE : MonoBehaviour
 {
-    private TrapBuff damageBuff;
-    private TrapBuff cooldownBuff;
-    private TrapBuff rangeBuff;
+    private TrapBuffAOEUpgrade upgrade;
 
     private void Awake()
     {
-        // Damage +50%
-        damageBuff = gameObject.AddComponent<TrapBuff>();
-        damageBuff.affectedStat = TrapStat.Damage;
-        damageBuff.percentBonus = 0.50f;
+        upgrade = GetComponentInParent<TrapBuffAOEUpgrade>();
 
-        // Cooldown -50%
-        cooldownBuff = gameObject.AddComponent<TrapBuff>();
-        cooldownBuff.affectedStat = TrapStat.Cooldown;
-        cooldownBuff.percentBonus = -0.50f;
-        
-        // Range +50%
-        rangeBuff = gameObject.AddComponent<TrapBuff>();
-        rangeBuff.affectedStat = TrapStat.Range;
-        rangeBuff.percentBonus = 0.50f;
+        if (upgrade == null)
+        {
+            Debug.LogError(
+                $"{name} could not find a TrapBuffAOEUpgrade in its parent."
+            );
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (upgrade == null)
+            return;
+
         Trap trap = other.GetComponent<Trap>();
 
         if (trap == null)
             return;
 
-        trap.AddBuff(damageBuff);
-        trap.AddBuff(cooldownBuff);
-        trap.AddBuff(rangeBuff);
+        trap.AddBuff(upgrade.GetDamageBuff());
+        trap.AddBuff(upgrade.GetCooldownBuff());
+        trap.AddBuff(upgrade.GetRangeBuff());
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (upgrade == null)
+            return;
+
         Trap trap = other.GetComponent<Trap>();
 
         if (trap == null)
             return;
 
-        trap.RemoveBuff(damageBuff);
-        trap.RemoveBuff(cooldownBuff);
-        trap.RemoveBuff(rangeBuff);
+        trap.RemoveBuff(upgrade.GetDamageBuff());
+        trap.RemoveBuff(upgrade.GetCooldownBuff());
+        trap.RemoveBuff(upgrade.GetRangeBuff());
     }
 }
